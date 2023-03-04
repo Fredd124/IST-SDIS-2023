7ca449cf32a2c4939b7a0a7603bf7d53ae9f4926
Frederico Silva <frederico.m.silva@tecnico.ulisboa.pt>
2023-03-04 18:16:22 +0000

    created stub and channel creation methods

--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -1,8 +1,34 @@
 package pt.tecnico.distledger.userclient.grpc;
 
+import pt.ulisboa.tecnico.distledger.contract.user.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+//One method for both stub and channel creation?
+
 public class UserService {
 
     /*TODO: The gRPC client-side logic should be here.
         This should include a method that builds a channel and stub,
         as well as individual methods for each remote operation of this service. */
+
+    public UserService() {}
+
+    public ManagedChannel creatChannel(String target) {
+        // Channel is the abstraction to connect to a service endpoint.
+        // Let us use plaintext communication because we do not have certificates.
+        final ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
+        return channel;
+    }
+
+    public UserServiceGrpc.UserServiceBlockingStub createStub(ManagedChannel channel) {
+        
+        // It is up to the client to determine whether to block the call.
+        // Here we create a blocking stub, but an async stub,
+        // or an async stub with Future are always possible.
+        UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
+    
+        return stub;
+    }
 }
