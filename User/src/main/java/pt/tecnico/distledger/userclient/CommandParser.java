57c18b7eb7b95e47ab4b532a38163463a0709411
Frederico Silva <frederico.m.silva@tecnico.ulisboa.pt>
2023-03-08 13:28:03 +0000

    fix: fixed new line

--- User/src/main/java/pt/tecnico/distledger/userclient/CommandParser.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/CommandParser.java
@@ -64,6 +64,7 @@ public class CommandParser {
                         break;
 
                     default:
+                        this.printUsage();
                         break;
                 }
             }
