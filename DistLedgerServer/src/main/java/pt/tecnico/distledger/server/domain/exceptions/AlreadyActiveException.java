4f949a0138e80811d5c518ded7fb33f4a26d193b
João Penedo <joao.penedo@tecnico.ulisboa.pt>
2023-03-08 13:24:08 +0000

    fix: fixed F's mistakes

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/AlreadyActiveException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/AlreadyActiveException.java
@@ -2,6 +2,6 @@ package pt.tecnico.distledger.server.domain.exceptions;
 
 public class AlreadyActiveException  extends ServerStateException {
     public AlreadyActiveException() {
-        super("The server provided is not active");
+        super("The server provided is already active");
     }  
 }
